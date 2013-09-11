607c9c9931609fd0580e49c7ce6fab10c09c929b
Matt Schaar <matt.schaar@gmail.com>
2013-09-10 21:48:54 -1000

    Added URL based image uploads

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -73,6 +73,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image)
+      params.require(:pin).permit(:description, :image, :image_remote_url)
     end
 end
